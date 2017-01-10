52c2d79085060fa553c74ea0766d5f85510cb092
ut <ulf.treger@googlemail.com>
2017-01-10 21:11:34 +0000

    home view improved, sorting by startdate, only future events for col 1 + 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,12 @@ class HomeController < ApplicationController
   end
 
   def index
-    @posts_programm = Post.where(:ptype => 'Programm')
-    @posts_neues = Post.where(:ptype => 'Neues im zakk')
-    @posts_undsonst = Post.where(:ptype => 'Und sonst?')
+    @posts_programm = Post.where(:ptype => 'Programm').where("startdate > ?", ( DateTime.now - 1.day)).order(:startdate)
+    #  p.startdate.day - DateTime.now.midnight.day
+    @posts_neues = Post.where(:ptype => 'Neues im zakk').order(:startdate)
+    @posts_undsonst = Post.where(:ptype => 'Und sonst?').where("startdate > ?", ( DateTime.now - 1.day)).order(:startdate)
     @current_program = Program.where('current = ?', true).first
 
   end
 
 end
-
