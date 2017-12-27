c2cbff7f835490c64839b11cd00f600eb3aeadc2
ut <ulf.treger@googlemail.com>
2017-12-27 17:59:36 +0000

    feature rspec login/logout

--- spec/features/admin/admin_programs_spec.rb
+++ spec/features/admin/admin_programs_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe "Admin::Programs" do
 
   describe "Guests" do
 
-    it "shows login form" do
-      visit new_user_session_path
-      click_button "Sign in"
+    it "shows login form", :js => false do
+      visit "/admin/programs"
+      expect(page).to have_content 'Sie müssen sich anmelden'
+    end
+
+    it "shows error if login with wrong credentials", :js => false do
+      visit '/admin/programs'
+      fill_in 'user_login', with: 'nobody'
+      fill_in 'user_password', with: 'abcdefghijklmn'
+      click_button I18n.t('devise.sessions.new.sign_in')
+      expect(page).to have_content 'Ungültige Anmeldedaten'
     end
 
   end
@@ -14,18 +22,26 @@ RSpec.describe "Admin::Programs" do
   describe "Users" do
     before do
       @user = FactoryGirl.create(:user)
-      # @user.save_without_session_maintenance
     end
 
+    it "login before accessing admin area", :js => false do
+      visit "/admin/programs"
+      expect(page).to have_current_path "/users/sign_in"
+      fill_in 'user_login', :with => @user.login
+      fill_in 'user_password', :with => @user.password
+      click_button I18n.t('devise.sessions.new.sign_in')
+      expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    end
 
-    it "receives some content w/capybara", :js => false do
+    it "logout", :js => false do
       visit "/admin/programs"
       fill_in 'user_login', :with => @user.login
       fill_in 'user_password', :with => @user.password
-      click_button 'Sign in'
-
+      click_button  I18n.t('devise.sessions.new.sign_in')
+      click_link('logout')
+      expect(page).to have_content I18n.t('devise.sessions.signed_out')
     end
 
-  end
 
+  end
 end
