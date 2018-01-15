2380ae26b705b97d520fd5e8482b67f9aec4541b
ut <ulf.treger@googlemail.com>
2018-01-15 17:26:16 +0000

    adding fixtures structure. still travis fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,20 @@ def load_em(fixtures_dir)
     unless File.basename(file) == "users.yml" && User.count > 0
       ActiveRecord::FixtureSet.create_fixtures(fixtures_dir, File.basename(file, '.*'))
     end
+    unless File.basename(file) == "roles.yml" && Role.count > 0
+      ActiveRecord::FixtureSet.create_fixtures(fixtures_dir, File.basename(file, '.*'))
+    end
   end
 end
 
 
 ## load general fixtures
 load_em("db/fixtures/seeds")
+
+## load content fixtures for UI testing
+if Rails.env == 'backstop_test'
+  load_em("db/fixtures/contents")
+end
 
 User.find_by_login('admin').roles  << Role.where( name: 'admin').first
 User.find_by_login('user').roles << Role.where( name: 'user').first
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,9 @@ require 'capybara/rails'
 
 require "selenium/webdriver"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+# Capybara.register_driver :chrome do |app|
+#  Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
