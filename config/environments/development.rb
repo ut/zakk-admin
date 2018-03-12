333ea7f0d13b7b9119ce939aee1b3ad751afe509
ut <ulf.treger@googlemail.com>
2018-03-12 16:13:39 +0000

    mail test settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: '127.0.0.1:3000' }
 
+  config.action_mailer.perform_deliveries    = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
