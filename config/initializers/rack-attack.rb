05c0cfd8703f1c50c4676409cae078b4ddffd7d8
ut <ulf.treger@googlemail.com>
2018-04-26 19:11:11 +0100

    rack attack config

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -2,12 +2,12 @@ Rack::Attack.throttle("logins/ip", limit: 12, period: 1.hour) do |req|
   req.ip if req.post? && req.path.start_with?("/users/sign_in")
 end
 
-Rack::Attack.throttle('limit logins per email', limit: 2, period: 60) do |req|
+Rack::Attack.throttle('limit logins per email', limit: 8, period: 1.hour) do |req|
   if req.post? && req.path.start_with?("/users/sign_in")
     req.params['email']
   end
 end
 
 ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, req|
-  SECURITY_LOGGER.info "Throttled #{req.env["rack.attack.match_discriminator"]}"
+  # LOGGER.info "Throttled #{req.env["rack.attack.match_discriminator"]}"
 end
