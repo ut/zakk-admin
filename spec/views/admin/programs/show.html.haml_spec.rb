2366d12eef43f5e2efee751ad559e7132b012924
ut <ulf.treger@googlemail.com>
2019-01-05 19:54:38 +0100

    fix rspec

--- spec/views/admin/programs/show.html.haml_spec.rb
+++ spec/views/admin/programs/show.html.haml_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "admin/programs/show", type: :view do
   before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user = FactoryBot.create(:user_with_admin_role)
+      sign_in user
     @program = FactoryBot.create(:program,  :title => "Title")
   end
 
