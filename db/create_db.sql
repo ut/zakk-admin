10df5c97dbbb5e894dcad86f629575c836a4da63
ut <ulf.treger@googlemail.com>
2020-01-19 18:38:44 +0100

    better desribe db setup

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,10 +1,15 @@
-# user
+# how to setup a postgres db for local development
+
+# create user
 createuser zakk_admin -P
 
-# test / development
+# create test / development databases
 createdb zakk_admin_test -E unicode
 createdb zakk_admin_development -E unicode
 
-# staging / production
-createdb zakk_admin_test -E unicode
-createdb zakk_admin4_development -E unicode
+# set user rights
+$ sudo su postgres
+$ psql
+= ALTER USER zakk_admin CREATEDB;
+= GRANT ALL PRIVILEGES ON DATABASE "zakk_admin_development" TO zakk_admin;
+= GRANT ALL PRIVILEGES ON DATABASE "zakk_admin_test" TO zakk_admin;
