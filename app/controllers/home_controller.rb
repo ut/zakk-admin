3656ae517781f76dabd726fc59a061be0b7dd5fb
ut <ulf.treger@googlemail.com>
2020-01-19 18:58:57 +0100

    consider only published events for 2nd row

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @events_programm = Event.where(:ptype => '0').where("startdate > ?", ( DateTime.now - 1.day)).order(:startdate)
     #  p.startdate.day - DateTime.now.midnight.day
     @events_neues = Event.where(:ptype => '1').order(:startdate)
+    @events_neues_published = Event.where(:ptype => '1').where(:status => 'published').order(:startdate)
     @events_undsonst = Event.where(:ptype => '2').where("startdate > ?", ( DateTime.now - 1.day)).order(:startdate)
 
     @pages = Page.where(published: true).where(in_menu: true)
