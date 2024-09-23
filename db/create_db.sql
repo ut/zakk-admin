211ef7f5f09a29de91a04411a795f26b5f7eadb7
Ulf Treger <ulf.treger@3plusx.de>
2024-09-23 22:24:06 +0200

    Config adaption

--- db/create_db.sql
+++ db/create_db.sql
@@ -3,13 +3,16 @@
 # create user
 createuser zakk_admin -P
 
-# create test / development databases
-createdb zakk_admin_test -E unicode
-createdb zakk_admin_development -E unicode
+only for local dev installs:
+$ ALTER ROLE zakk_admin4_test SUPERUSER;
+
+# test / development
+createdb zakk_admin4_test -E unicode
+createdb zakk_admin4_development -E unicode
 
 # set user rights
 $ sudo su postgres
 $ psql
-= ALTER USER zakk_admin CREATEDB;
+= ALTER USER zakk_admin4 CREATEDB;
 = GRANT ALL PRIVILEGES ON DATABASE "zakk_admin_development" TO zakk_admin;
 = GRANT ALL PRIVILEGES ON DATABASE "zakk_admin_test" TO zakk_admin;
