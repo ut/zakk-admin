e92ce833bae410eea0a381296868ae062ea29ab7
Ulf Treger <ulf.treger@3plusx.de>
2024-10-01 17:35:39 +0200

    Sort flyers by updated_at as default. some css

--- app/models/flyer.rb
+++ app/models/flyer.rb
@@ -2,12 +2,9 @@ class Flyer < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  # crashes controller rspecs:
-  # validates_presence_of :image
-
   validates_integrity_of :image
   validates_processing_of :image
 
-
+  default_scope { order(updated_at: :desc) }
 
 end
